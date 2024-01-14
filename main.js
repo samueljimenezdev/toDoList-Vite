bf9956a0029e5ad2c3bb595005c140d83a186e7f
samueljimenezdev <sajimenezr1@gmail.com>
2024-01-14 13:11:46 -0500

    functions in store added

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ import todoStore from './src/store/todo.store.js'
 App('#app');
 
 todoStore.initStore();
+//todoStore.deleteCompletedTodos()
+console.log(todoStore.getTodos())
--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -6,6 +6,7 @@ const Filters = {
     Pending: 'Pending'
 }
 
+
 const state = {
     todos:[
         new Todo('Levantarse'),
@@ -22,7 +23,65 @@ const initStore = () => {
     console.log(state)
 }
 
+const loadStore = () =>{
+    throw new Error('Not implemented');
+}
+
+const addTodo = (description) =>{
+    if (!description) throw new Error('The description is required,');
+    state.todos.push( new Todo(description));
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch( filter ){
+        case Filters.All:
+            return {...state.todos};
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        default:
+            throw new Error(`Option ${filter} is not valid.`)
+    }
+}
+
+const toggleTodo = (todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = (todoId) =>{
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompletedTodos = () =>{
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+const setSelectedFilter = (newFilter = Filters.All) => {
+    if (Object.keys(Filters).includes(newFilter)){
+        state.filter = newFilter;
+    }else{
+        throw new Error(`The filter ${newFilter} doesn't exist.`)
+    }
+
+
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
+
 
 export default{
-    initStore
+    initStore,
+    loadStore,
+    addTodo,
+    getTodos,
+    toggleTodo,
+    deleteTodo,
+    deleteCompletedTodos,
+    setSelectedFilter,
+    getCurrentFilter,
 }
