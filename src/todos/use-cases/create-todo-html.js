85b9eb3d0ddf0d5de504449e62516ba79f670c60
samueljimenezdev <sajimenezr1@gmail.com>
2024-01-15 21:28:13 -0500

    render individual task

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -1,10 +1,19 @@
-import Todo from '../models/todo.model.js'
+import Todo from "../models/todo.model.js";
 
-export const createTodoHTML = ( todo ) => {
-    if (!todo) throw new Error('A TODO object is required');
-    const html = `<h1>${ todo.description }</h1>`;
-    const liElement = document.createElement('li');
-    console.log(liElement)
-    liElement.innerHTML = html;
-    return liElement;
-}
+export const createTodoHTML = (todo) => {
+  if (!todo) throw new Error("A TODO object is required");
+  const html = `
+    <div class="view">
+        <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
+        <label>${ todo.description }</label>
+        <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">`;
+  const liElement = document.createElement("li");
+  liElement.setAttribute('data-id', todo.id);
+  if (todo.done){
+    liElement.classList.add('completed');
+  }
+  liElement.innerHTML = html;
+  return liElement;
+};
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,15 +1,20 @@
 import Todo from '../models/todo.model';
 import { createTodoHTML } from './create-todo-html';
 
+let element;
 /**
  * 
  * @param {string} elementId 
  * @param {Todo} todos 
  */
 export const renderTodos = (elementId, todos = []) => {
+    if (!element){
+        element = document.querySelector( elementId );
+    }
+    if (!element) throw new Error(`Element ${elementId} not found`)
 
-    const element = document.querySelector( elementId );
-
+    element.innerHTML = '';
+    
     todos.forEach( todo => {
         element.append( createTodoHTML( todo ))
     });
